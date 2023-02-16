877605c4eb8a6b4d8a08dc1eb7bcd3b365f05b7a
Vitor Kubinyete <vitorkubinyete@hotmail.com>
2023-02-16 12:44:31 -0300

    Updated production URL

--- src/Core/LogbankEnvironment.php
+++ src/Core/LogbankEnvironment.php
@@ -5,7 +5,7 @@ namespace Kubinyete\Logbank\Core;
 class LogbankEnvironment extends Environment
 {
     private const URL_SANDBOX       = 'https://techsavvy-api-sandbox.secure.srv.br/api/v1';
-    private const URL_PRODUCTION    = 'https://techsavvy-api.secure.srv.br/api/v1';
+    private const URL_PRODUCTION    = 'https://api-tsv.secure.srv.br/api/v1';
 
     public static function sandbox(): LogbankEnvironment
     {
